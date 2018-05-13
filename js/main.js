e145ddcb41a0306c3b346588cb7e04b5b53dbe4e
Ronnie Bjørkelund <ronnie.bjorkelund@gmail.com>
2018-05-14 01:17:05 +0200

    design and functionality with dummy data complete

--- js/main.js
+++ js/main.js
@@ -1,21 +1,49 @@
 const searchBtn = document.getElementById("searchBtn");
+const outputSection = document.getElementById("output");
 
-searchBtn.addEventListener("click", function(e) {
+
+searchBtn.addEventListener("click", function (e) {
   if (e.preventDefault) {
     e.preventDefault(); // prevent page from reloading...
   }
-  searchField = document.getElementById("searchField").value;
-  document.getElementById("output").innerHTML = searchField;
+  let searchString = document.getElementById("searchField").value;
+
+  if (searchString == "") {
+
+    clearOutputSection();
+
+  } else {
+
+
+    writeToOutputSection(getDummyArticle());
+  }
 
   return false;
 });
 
 
+function getDummyArticle() {
+  let dummyArticle = document.createElement("article");
+  dummyArticle.innerHTML = "<h3>loremtitle</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure laboriosam quae ipsam eos veritatis fuga rem quasi vel odit voluptates a nam perspiciatis sint assumenda, cumque cum doloremque. Neque, eveniet!</p>";
+  return dummyArticle;
+}
+
+function clearOutputSection() {
+  let titleText = "<h2>Results</h2>";
+  outputSection.innerHTML = titleText;
+  outputSection.style.display = "none";
+}
+
+function writeToOutputSection(article) {
+  outputSection.appendChild(article);
+  outputSection.style.display = "block";
+}
+
 /*
 function processForm(e) {
   if (e.preventDefault) e.preventDefault();
 
-   do what you want with the form 
+   do what you want with the form
 
    You must return false to prevent the default form behavior
   return false;
